a397c70bec13e7a577ff4d41a84218d939dd145e
Matias_ <94906092+MaestroQ@users.noreply.github.com>
2021-12-08 10:30:40 +0200

    MainActivity visuaalinen malli, koskettu myös AndroidManifestiin ja Mainin javaan logon asentamiseksi.

--- app/src/main/java/com/example/project_420/MainActivity.java
+++ app/src/main/java/com/example/project_420/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.app_logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
 }
