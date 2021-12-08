f982499c7956a0988188b72a2aa26a855cdad75c
tino_ <tino.behnen@metropolia.fi>
2021-12-08 10:16:46 +0200

    Activity for plan week 1.1: Made into a singleton

--- app/src/main/java/com/example/project_420/Week.java
+++ app/src/main/java/com/example/project_420/Week.java
@@ -1,29 +1,39 @@
 package com.example.project_420;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Week
 {
-    private String name, description;
-    private ArrayList week;
+    private List<Movement> movements;
+    private static final Week ourInstance = new Week();
 
-    public Week(String name, String description)
+    public static Week getInstance()
     {
-        this.name = name;
-        this.description = description;
-        week = new ArrayList<Movement>();
+        return ourInstance;
+    }
+
+    private Week()
+    {
+        this.movements = new ArrayList<>();
     }
 
     public void addMovement(Movement movement)
     {
-        week.add(movement);
+        this.movements.add(movement);
     }
 
-    public ArrayList getDay(Day day)
+    public List<Movement> movements()
+    {
+        return this.movements;
+    }
+
+    public ArrayList<Movement> getDay(Day day)
     {
         ArrayList dayList = new ArrayList<Movement>();
 
-        for (Movement m : this.week){
+        for (Movement m : this.movements)
+        {
             if (m.getDay() == day)
                 dayList.add(m);
         }
